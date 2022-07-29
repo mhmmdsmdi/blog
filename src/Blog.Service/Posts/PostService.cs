f039c9ddd6dc705b495ae7a809df98436229e93e
Mohammad Samadi <mhmmdsmdi@gmail.com>
2022-07-30 02:45:15 +0430

    feat: create post service

---
+++ src/Blog.Service/Posts/PostService.cs
@@ -0,0 +1,34 @@
+﻿using Blog.Common;
+using Blog.Shared.Models.Posts.Dtos;
+
+namespace Blog.Service.Posts;
+
+public interface IPostService : IService
+{
+    Task<List<PostSelectDto>> GetPosts(CancellationToken cancellationToken);
+}
+
+public class PostService : IPostService
+{
+    public Task<List<PostSelectDto>> GetPosts(CancellationToken cancellationToken)
+    {
+        return Task.FromResult(new List<PostSelectDto>()
+        {
+            new()
+            {
+                Title = "پست شماره 1",
+                Content = "محتوای پست",
+            },
+            new()
+            {
+                Title = "پست شماره 2",
+                Content = "محتوای پست",
+            },
+            new()
+            {
+                Title = "پست شماره 3",
+                Content = "محتوای پست",
+            },
+        });
+    }
+}
---
+++ src/Shared/Models/Posts/Dtos/PostSelectDto.cs
@@ -0,0 +1,16 @@
+﻿using Blog.Common.Dto;
+
+namespace Blog.Shared.Models.Posts.Dtos;
+
+public class PostSelectDto : BaseDto<PostSelectDto, Post>
+{
+    /// <summary>
+    /// عنوان
+    /// </summary>
+    public string Title { get; set; }
+
+    /// <summary>
+    /// محتوا
+    /// </summary>
+    public string Content { get; set; }
+}
